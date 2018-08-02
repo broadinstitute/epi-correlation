e9f438ea95e62f684757254e6c40ceb5ce72f8ea
infispiel <infinite.loopholes@gmail.com>
2018-08-02 12:41:00 -0400

    Parallelize part of pipeline & add STDOUT flag
    Parallelized the longest parts of the pipeline
    (indexing, IGVCount)
    
    Added flag to print correlation to stdout.
    Added flag to define where correlation is saved,
    if not echo'd to stdout

--- scripts/runEntirePipeline.sh
+++ scripts/runEntirePipeline.sh
@@ -15,22 +15,27 @@ set -e
 inLoc_1=1 # Location of 1st Bam
 inLoc_2=1 # Location of 2nd Bam
 readLen=0 # Read length, so we can calculate extension factor later
-outputLoc="/tmp/" # Folder where to store all of the results
+midLoc="/tmp/" # Folder where to store all of the midway files (i.e. coverage.wig, coverage_processed.wig.PARAMS)
+outLoc=""
 # TODO: 2 options>
 isPE=false # Are the bams paired end?
 debug=false # Is debug mode on?
+printToStdOut=false
 # TODO: Nickname parameter? (instead of coverage[etc], cov_17767[etc] ... )
 
-usage() { echo "Usage: $0 [-p|-l <0-200>] -a <input bam> -b <input bam> -o </tmp/>" 1>&2; exit 1;}
+usage() { echo "Usage: $0 [-p|-l <0-200>] -a <input bam> -b <input bam> -m </tmp/> [-o <>] [-s]" 1>&2; exit 1;}
 # Reusing some logic from runCount.sh
 #   key points are: input (bam file), output (folder), l/p (paired or read length)
-while getopts "h?pl:a:b:o:d" o; do
+while getopts "h?pl:a:b:m:do:s" o; do
     case "${o}" in
 	d)
 	    debug=true
 	    ;;
     p)
         isPE=true
+        ;;
+    s)
+        printToStdOut=true
         ;;
     h|\?)
         usage
@@ -45,9 +50,12 @@ while getopts "h?pl:a:b:o:d" o; do
     l)
         readLen=$OPTARG
         ;;
-	o)
-	    outputLoc=$OPTARG
+	m)
+	    midLoc=$OPTARG
 	    ;;
+    o)
+        outLoc=$OPTARG
+        ;;
     :)
         echo "Option -$OPTARG requires an argument." >&2
 	    usage
@@ -84,29 +92,33 @@ else
     args="-l ${readLen}"
 fi
 
-run_pipeline ()
+check_for_bais ()
 {
-    # Check if bam files are indexed
-
     if [[ ! -f ${1}.bai  && ! -f ${1%.*}.bai ]]; then
         if [[ $debug == true ]]; then echo "Creating index file for ${1}"; fi
         igvtools index ${1}
     fi
+}
+
+run_pipeline ()
+{
+    # Check if bam files are indexed
 
     if [[ $debug == true ]]; then echo "Running IGVTools Count for ${1}; saving to ${2}"; fi
-    /scripts/runCount.sh ${args} -i ${1} -o ${outputLoc}${2}.wig
+    /scripts/runCount.sh ${args} -i ${1} -o ${midLoc}${2}.wig
 
     if [[ $debug == true ]]; then echo "Fixing Coverage File ${2}"; fi
-    /scripts/fixCoverageFiles.sh -i ${outputLoc}${2}.wig -o ${outputLoc}${2}_processed.wig
+    /scripts/fixCoverageFiles.sh -i ${midLoc}${2}.wig -o ${midLoc}${2}_processed.wig
 
     if [[ $debug == true ]]; then echo "Fitting distribution to ${2}"; fi
-    Rscript /scripts/fitDistribution.R --input_loc ${outputLoc}${2}_processed.wig --output_loc ${outputLoc}${2}_p_value.wig
+    Rscript /scripts/fitDistribution.R --input_loc ${midLoc}${2}_processed.wig --output_loc ${midLoc}${2}_p_value.wig
 
     if [[ $debug == true ]]; then echo "Fitting pipeline complete on $1"; fi
 }
 
-run_pipeline ${inLoc_1} coverage_a
-run_pipeline ${inLoc_2} coverage_b
+check_for_bais ${inLoc_1} & check_for_bais ${inLoc_2} & wait
+run_pipeline ${inLoc_1} coverage_a & run_pipeline ${inLoc_2} coverage_b & wait
 
-cor=$( Rscript /scripts/findCorrelation.R --wig1 ${outputLoc}coverage_a_p_value.wig --wig2 ${outputLoc}coverage_b_p_value.wig )
-echo ${cor} > "cor_out.txt"
+cor=$( Rscript /scripts/findCorrelation.R --wig1 ${midLoc}coverage_a_p_value.wig --wig2 ${midLoc}coverage_b_p_value.wig )
+if [[ $printToStdOut == true ]]; then echo $cor; exit 0; fi
+echo ${cor} > $outLoc"cor_out.txt"
