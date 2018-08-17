8aee6ff7888779456bf4a7d5e3c216ebe662058a
infispiel <infinite.loopholes@gmail.com>
2018-08-17 11:13:39 -0400

    Add documentation & Fix SED error

--- scripts/guaranteeBins.R
+++ scripts/guaranteeBins.R
@@ -2,10 +2,20 @@ suppressPackageStartupMessages(library(dplyr))
 
 # Combines the processed wig with the reference bins_template.bed file, ensuring that we have only the bins present in bins_template.
 # This is important for a lot of our later processes.
-# TODO : This could be neater.
-args= commandArgs(trailingOnly=TRUE)
-bin_bl <- read.table("/reference/bins_template.bed", stringsAsFactors = F)
-new_cov <- read.table(args[1], stringsAsFactors = F)
-final_cov <- left_join(bin_bl, new_cov, by=c("V1","V2")) # V1,V2 = chr, start
-final_cov[which(is.na(final_cov$V3)),"V3"] = 0 # V3 = count
-write.table(args[2],x=final_cov,row.names = F, col.names = F,quote=F)
+# Get Input (args[1]) & Output (args[2]) location
+args=commandArgs(trailingOnly=TRUE)
+
+# Our bin template, produced elsewhere & provided with this docker
+bins_template <- read.table("/reference/bins_template.bed", stringsAsFactors = F)
+colnames(bins_template) <- c("chr","start")
+
+# Our reformatted BED file, containing 5kb regions and their coverage
+sparse_coverage <- read.table(args[1], stringsAsFactors = F)
+colnames(sparse_coverage) <- c("chr","start","count")
+
+# Left join to ensure we have all of & only the bins in bins_template; set count to 0 where NA
+final_coverage <- left_join(bins_template, sparse_coverage, by=c("chr","start"))
+final_coverage[which(is.na(final_coverage$count)),"count"] = 0
+
+# Save
+write.table(args[2],x=final_coverage,row.names = F, col.names = F,quote=F)
