b73c3f071b68ce9658ec5aa452f1ee0dc710056c
infispiel <infinite.loopholes@gmail.com>
2018-08-21 12:35:41 -0400

    Fix typo in Java logging

--- scripts/runCount.sh
+++ scripts/runCount.sh
@@ -75,5 +75,5 @@ then
     exit 0
 fi
 # Otherwise, actually run igvtools count.>
-igvtools count -w 5000 --minMapQuality 1 ${args} ${BAMLoc} ${outputLoc} hg19 > /tmp/${BAMLOC}_log.txt 2>/tmp/${BAMLIC}_log.txt
+igvtools count -w 5000 --minMapQuality 1 ${args} ${BAMLoc} ${outputLoc} hg19 > /tmp/${BAMLOC}_log.txt 2>/tmp/${BAMLOC}_log.txt
 exit 0
