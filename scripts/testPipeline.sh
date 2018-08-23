ff60fec8a00eeff147da2ff926d56b85c77b3e45
infispiel <infinite.loopholes@gmail.com>
2018-08-23 14:26:03 -0400

    Add error exit mode on test failure

--- scripts/testPipeline.sh
+++ scripts/testPipeline.sh
@@ -8,8 +8,22 @@ cp /test_data/*.bam /data/
 differentFiles=$(/scripts/runEntirePipeline.sh -p -a /data/TEST_A.bam -b /data/TEST_B.bam -c)
 sameFiles=$(/scripts/runEntirePipeline.sh -p -a /data/TEST_A.bam -b /data/TEST_A.bam -c)
 echo "Pipeline testing results: "
+
 echo "File A vs File A: "
-echo -n ${sameFiles}; echo -n "     "; [[ ${sameFiles} =~ "1" ]] && echo "Pass" || echo "Fail"
+echo -n ${sameFiles}; echo -n "     "
+if [[ ${sameFiles} =~ "1" ]]; then 
+  echo "Pass"
+else
+  echo "Fail"
+  exit 1
+fi
 echo
+
 echo "File A vs File B: "
-echo -n ${differentFiles}; echo -n "     "; [[ ${differentFiles} =~ "-0.652163" ]] && echo "Pass" || echo "Fail"
+echo -n ${differentFiles}; echo -n "     "
+if [[ ${differentFiles} =~ "-0.652163" ]]; then 
+  echo "Pass"
+else
+  echo "Fail"
+  exit 1
+fi
